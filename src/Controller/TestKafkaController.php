a96589a88ea6f1eea05d80c8322322ef5f2cc862
Schizy <ali.mechichi@gmail.com>
2025-10-06 13:04:18 +0200

    Removes unused AbstractController

--- src/Controller/TestKafkaController.php
+++ src/Controller/TestKafkaController.php
@@ -3,11 +3,10 @@
 namespace App\Controller;
 
 use App\Service\KafkaProducer;
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
-use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Routing\Attribute\Route;
 
-class TestKafkaController extends AbstractController
+class TestKafkaController
 {
     #[Route('/send-kafka')]
     public function send(KafkaProducer $producer): Response
